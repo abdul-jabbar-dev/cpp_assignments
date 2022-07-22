c6230e6babf3e2a9990c61186d85a4f7e761799d
abduljabbar15 <abduljabber1532002@gmail.com>
2022-07-23 02:11:05 +0600

    complete assignment

--- 8-1_Theory_Exam_01.cpp
+++ 8-1_Theory_Exam_01.cpp
@@ -9,7 +9,7 @@ public:
     int food_item_prices[12];
 
 private:
-    int total_tax;
+    float total_tax;
 
 public:
     Restaurant(int *food_item_codes, string *food_item_names, int *food_item_prices)
@@ -20,6 +20,16 @@ public:
             this->food_item_names[i] = food_item_names[i];
             this->food_item_prices[i] = food_item_prices[i];
         }
+        this->total_tax = 0;
+    };
+    void update_tax(float prev_tax)
+    {
+
+        this->total_tax += prev_tax;
+    };
+    float get_total_tax()
+    {
+        return this->total_tax;
     };
 };
 
@@ -40,21 +50,21 @@ Restaurant *create_restaurant(int n)
 };
 int filter(Restaurant *restaurant, int temp)
 {
-    for (int j = 0; j < sizeof(restaurant->food_item_codes) / sizeof(restaurant->food_item_codes[0]); j++)
+    for (int j = 0; j < sizeof(restaurant->food_item_codes) / sizeof(int); j++)
     {
         if (restaurant->food_item_codes[j] == temp)
             return temp;
     };
-    cout << temp << " Incorrect item code try again: ";
+    cout << temp << " item not found try again: ";
     cin >> temp;
     filter(restaurant, temp);
 }
 void get_menu(Restaurant *restaurant)
 {
-    cout << "                        Make bill                      " << endl;
+    cout << "                   AVAILABLE ITEMS                     " << endl;
     cout << "_______________________________________________________" << endl;
-    cout << "_______________________________________________________" << endl;
-    cout << "Code:\t\tName:\t\t\t\tPrice: " << endl;
+    cout << "-------------------------------------------------------" << endl;
+    cout << "CODE:\t\tNAME:\t\t\t\tPRICE: " << endl;
     cout << endl;
     for (int i = 0; i < 12; i++)
     {
@@ -70,27 +80,48 @@ void get_menu(Restaurant *restaurant)
 
 void my_order(int table_no, int number_of_items, int *item_code, int *item_quantity, Restaurant *restaurant)
 {
-    cout << "\t\t\t\t Bill summary";
-    cout << "Table no. " << number_of_items;
-    cout << "Code:\t\tName:\t\t\t\tQuantity:\t\t\tPrice: " << endl;
+    int sub_total = 0;
+    float total_tax = 0;
+    cout << endl;
+    cout << endl;
+    cout << "\t\t\t\t BILL SUMMARY" << endl;
+    cout << "--------------------------------------------------------------------------------" << endl;
+
+    cout << "Table no. " << number_of_items << endl;
+    cout << endl;
+    cout << "CODE:\t\tNAME:\t\t\t\tRATE:\tQUANTITY: \tTOTAL: " << endl;
+    cout << endl;
+
     for (int i = 0; i < number_of_items; i++)
     {
-        for (int l = 0; l < sizeof(restaurant->food_item_codes) / sizeof(restaurant->food_item_codes[0]); l++)
+        for (int l = 0; l < sizeof(restaurant->food_item_codes) / sizeof(int); l++)
         {
-            if (number_of_items[i]== restaurant->food_item_codes[l])
+            if (item_code[i] == restaurant->food_item_codes[l])
             {
-                cout << restaurant->food_item_codes[l] << "\t" << restaurant->food_item_names[l]<<endl;
+                sub_total += (item_quantity[i]) * (restaurant->food_item_prices[l]);
+                cout << restaurant->food_item_codes[l] << "\t\t" << restaurant->food_item_names[l] << "\t";
+                cout << "\t" << restaurant->food_item_prices[l] << "\t" << item_quantity[i] << "\t\t" << (item_quantity[i]) * (restaurant->food_item_prices[l]) << endl;
             }
-            
         }
-        
     }
+    float tax_rate = (float)sub_total * 5 / 100;
+    cout << endl;
+    cout << "                                                                  --------------" << endl;
+    cout << "Total                                                             \t" << sub_total << endl;
+    cout << "TAX                                                               \t" << tax_rate << endl;
+    cout << "--------------------------------------------------------------------------------" << endl;
+    cout << "NET TOTAL\t\t\t\t\t\t\t\t" << sub_total + tax_rate << " Taka" << endl;
+    restaurant->update_tax(tax_rate);
+    float res_total_tax = restaurant->get_total_tax();
+    // cout << res_total_tax; total tax
 }
 
 void user_order(Restaurant *restaurant)
 {
     int table_no, number_of_items, item_code[number_of_items], item_quantity[number_of_items];
-    cout << "Welcome to our restaurant" << endl;
+    cout << endl;
+    cout << "\t\tWelcome to our restaurant" << endl;
+    cout << "\t-----------------------------------------" << endl;
     cout << endl;
     cout << "Table Number: ";
     cin >> table_no;
@@ -102,10 +133,10 @@ void user_order(Restaurant *restaurant)
     for (int i = 0; i < number_of_items; i++)
     {
         int temp;
-        cout << i << " Item code: ";
+        cout << i + 1 << " Item code: ";
         cin >> temp;
         item_code[i] = filter(restaurant, temp);
-        cout << i << " How many you want (Quantity): ";
+        cout << i + 1 << " How many you want (Quantity): ";
         cin >> item_quantity[i];
     };
     my_order(table_no, number_of_items, item_code, item_quantity, restaurant);
@@ -117,7 +148,10 @@ int main()
     Restaurant *restaurant;
     restaurant = create_restaurant(n);
     get_menu(restaurant);
-    user_order(restaurant);
+    while (1)
+    {
+        user_order(restaurant);
+    }
 
     return 0;
 }
